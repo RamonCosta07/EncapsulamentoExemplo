f142023f61a2639163904248189ebe643764f77d
RamonCosta07 <ramonnar20@hotmail.com>
2021-12-27 22:54:55 -0300

    Primeiro exemplo de encapsulamento

---
+++ EncapsulamentoExemplo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EncapsulamentoExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // valores válidos
+            Retângulo r = new Retângulo();
+            r.DefinirMedidas(30, 30);
+            Console.WriteLine($"Área de R: {r.ObterArea()}");
+            Console.WriteLine("**************************");
+
+            //valores inválidos
+            Retângulo s = new Retângulo();
+            s.DefinirMedidas(-30, 0);
+            Console.WriteLine($"Área de S: {s.ObterArea()}");
+            Console.WriteLine("**************************");
+
+        }
+    }
+}
---
+++ EncapsulamentoExemplo/Retângulo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulamentoExemplo
+{
+    class Retângulo
+    {
+        private double comprimento;
+        private double largura;
+        private bool valido; // valor padrão de todo booleano é falso
+
+        public void DefinirMedidas(double comprimento, double largura)
+        {
+            if (comprimento > 0 && largura > 0)
+            {
+                this.comprimento = comprimento;
+                this.largura = largura;
+                valido = true;
+            } else
+            {
+                Console.WriteLine("Valores inválidos");
+            }
+        }
+
+        public double ObterArea()
+        {
+            if (valido)
+            {
+                return comprimento * largura;
+            }
+            else
+            {
+                Console.WriteLine("Preencha valores válidos");
+                return 0;
+            }
+        }
+    }
+}
